33d8e473f788c03b994e57428d9b4bd6c7e2dfdc
insu1213 <insu12345_@naver.com>
2023-06-28 15:14:07 +0900

    update: MainActivity.kt 컴포즈 공식문서

--- app/src/main/java/com/insu/compose/MainActivity.kt
+++ app/src/main/java/com/insu/compose/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.insu.compose
 
+import android.content.res.Configuration
+import android.graphics.drawable.Icon
 import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.annotation.DrawableRes
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,39 +32,111 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("조이, 여명의 성위", "야호! 재밌겠다. 그치?"))
+            ComposeExampleTheme {
+                Column {
+                    SearchBar()
+                    Surface(modifier = Modifier.fillMaxSize()) {
+                        Conversation(SampleData.conversationSample)
+                    }
+                }
+            }
         }
     }
 }
 
-data class Message(val author: String, val body: String)
+data class Message(val author: String, val body: String, @DrawableRes val picture: Int)
 
 @Composable
 fun MessageCard(msg: Message) {
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
-            painter = painterResource(R.drawable.zoe),
+            painter = painterResource(msg.picture),
             contentDescription = "zoe img",
+            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
-        
+
         Spacer(modifier = Modifier.width(8.dp))
-        
-        Column {
-            Text(text = msg.author)
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if(isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+
+            )
+
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if(isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
-    
 }
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon =  {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        ),
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+    )
+}
+
+//@Preview(name = "Light Mode")
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_YES,
+//    showBackground = true,
+//    name = "Dark Mode"
+//)
 
 @Preview
 @Composable
-fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("조이, 여명의 성위", "야호! 재밌겠다. 그치?")
-    )
+fun PreviewConversation() {
+    ComposeExampleTheme {
+        Conversation(SampleData.conversationSample)
+    }
 }
---
+++ app/src/main/java/com/insu/compose/SampleData.kt
@@ -0,0 +1,53 @@
+package com.insu.compose
+
+class SampleData {
+    companion object {
+        val conversationSample: List<Message> = listOf(
+            Message(
+                "조이, 여명의 성위",
+                "난 조이야. 넌 이름이 뭐니?",
+                R.drawable.zoe
+            ),
+            Message(
+                "람머스, 중무장 아르마딜로",
+                "그래.",
+                R.drawable.rammus
+            ),
+            Message(
+                "조이, 여명의 성위",
+                "야호! 재밌겠다. 그치?",
+                R.drawable.zoe
+            ),
+            Message(
+              "이즈리얼, 무모한 탐험가",
+              "임무를 수락하지! 잠깐 어디가는 거라고?",
+                R.drawable.ezreal
+            ),
+            Message(
+                "조이, 여명의 성위",
+                "너희 모두를 위한 소식을 가져왔어. 일종의 경고랄까? 그런데 우선 반짝이가 보고 싶어!",
+                R.drawable.zoe
+            ),
+            Message(
+                "럭스, 광명의 소녀",
+                "어디 한번 길을 밝혀보죠!",
+                R.drawable.lux
+            ),
+            Message(
+                "조이, 여명의 성위",
+                "세상에! 이런 법이 어딨어!",
+                R.drawable.zoe
+            ),
+            Message(
+                "아리, 구미호",
+                "똑똑한 여우는 절대 잡히지 않거든.",
+                R.drawable.ahri
+            ),
+            Message(
+                "조이, 여명의 성위",
+                "현실에 대한 착각들을 하잖아? 근데 그게 사실은 현실이 아니거든. 마치 안에도 옆에도 있는 것처럼 말이야. 근데 위에는 없거든. 아유, 됐어. 농담이야. 사실은 동담 아닌데.",
+                R.drawable.zoe
+            )
+        )
+    }
+}
