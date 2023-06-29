a36219af6041a71a302d389ac69e0748472b2cdd
insu1213 <insu12345_@naver.com>
2023-06-29 16:13:49 +0900

    sync: 20230629-01 *error

--- app/src/main/java/com/insu/compose/MainActivity.kt
+++ app/src/main/java/com/insu/compose/MainActivity.kt
@@ -32,111 +32,112 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeExampleTheme {
-                Column {
-                    SearchBar()
-                    Surface(modifier = Modifier.fillMaxSize()) {
-                        Conversation(SampleData.conversationSample)
-                    }
-                }
-            }
+//            ComposeExampleTheme {
+//                Column {
+//                    SearchBar()
+//                    Surface(modifier = Modifier.fillMaxSize()) {
+//                        Conversation(SampleData.conversationSample)
+//                    }
+//                }
+//            }
+            MyApp()
         }
     }
 }
 
-data class Message(val author: String, val body: String, @DrawableRes val picture: Int)
-
-@Composable
-fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(msg.picture),
-            contentDescription = "img",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
-        )
-
-        Spacer(modifier = Modifier.width(8.dp))
-
-        var isExpanded by remember { mutableStateOf(false) }
-        val surfaceColor by animateColorAsState(
-            if(isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
-        )
-
-        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colors.secondaryVariant,
-                style = MaterialTheme.typography.subtitle2
-
-            )
-
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Surface(
-                shape = MaterialTheme.shapes.medium,
-                elevation = 1.dp,
-                color = surfaceColor,
-                modifier = Modifier
-                    .animateContentSize()
-                    .padding(1.dp)
-            ) {
-                Text(
-                    text = msg.body,
-                    modifier = Modifier.padding(all = 4.dp),
-                    maxLines = if(isExpanded) Int.MAX_VALUE else 1,
-                    style = MaterialTheme.typography.body2
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun Conversation(messages: List<Message>) {
-    LazyColumn {
-        items(messages) { message ->
-            MessageCard(message)
-        }
-    }
-}
-
-@Composable
-fun SearchBar(
-    modifier: Modifier = Modifier
-) {
-    TextField(
-        value = "",
-        onValueChange = {},
-        leadingIcon =  {
-            Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = null
-            )
-        },
-        colors = TextFieldDefaults.textFieldColors(
-            backgroundColor = MaterialTheme.colors.surface
-        ),
-        modifier = modifier
-            .fillMaxWidth()
-            .heightIn(min = 56.dp)
-    )
-}
-
-//@Preview(name = "Light Mode")
-//@Preview(
-//    uiMode = Configuration.UI_MODE_NIGHT_YES,
-//    showBackground = true,
-//    name = "Dark Mode"
-//)
-
-@Preview
-@Composable
-fun PreviewConversation() {
-    ComposeExampleTheme {
-        Conversation(SampleData.conversationSample)
-    }
-}
+//data class Message(val author: String, val body: String, @DrawableRes val picture: Int)
+//
+//@Composable
+//fun MessageCard(msg: Message) {
+//    Row(modifier = Modifier.padding(all = 8.dp)) {
+//        Image(
+//            painter = painterResource(msg.picture),
+//            contentDescription = "img",
+//            contentScale = ContentScale.Crop,
+//            modifier = Modifier
+//                .size(40.dp)
+//                .clip(CircleShape)
+//                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+//        )
+//
+//        Spacer(modifier = Modifier.width(8.dp))
+//
+//        var isExpanded by remember { mutableStateOf(false) }
+//        val surfaceColor by animateColorAsState(
+//            if(isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+//        )
+//
+//        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+//            Text(
+//                text = msg.author,
+//                color = MaterialTheme.colors.secondaryVariant,
+//                style = MaterialTheme.typography.subtitle2
+//
+//            )
+//
+//            Spacer(modifier = Modifier.height(4.dp))
+//
+//            Surface(
+//                shape = MaterialTheme.shapes.medium,
+//                elevation = 1.dp,
+//                color = surfaceColor,
+//                modifier = Modifier
+//                    .animateContentSize()
+//                    .padding(1.dp)
+//            ) {
+//                Text(
+//                    text = msg.body,
+//                    modifier = Modifier.padding(all = 4.dp),
+//                    maxLines = if(isExpanded) Int.MAX_VALUE else 1,
+//                    style = MaterialTheme.typography.body2
+//                )
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun Conversation(messages: List<Message>) {
+//    LazyColumn {
+//        items(messages) { message ->
+//            MessageCard(message)
+//        }
+//    }
+//}
+//
+//@Composable
+//fun SearchBar(
+//    modifier: Modifier = Modifier
+//) {
+//    TextField(
+//        value = "",
+//        onValueChange = {},
+//        leadingIcon =  {
+//            Icon(
+//                imageVector = Icons.Default.Search,
+//                contentDescription = null
+//            )
+//        },
+//        colors = TextFieldDefaults.textFieldColors(
+//            backgroundColor = MaterialTheme.colors.surface
+//        ),
+//        modifier = modifier
+//            .fillMaxWidth()
+//            .heightIn(min = 56.dp)
+//    )
+//}
+//
+////@Preview(name = "Light Mode")
+////@Preview(
+////    uiMode = Configuration.UI_MODE_NIGHT_YES,
+////    showBackground = true,
+////    name = "Dark Mode"
+////)
+//
+//@Preview
+//@Composable
+//fun PreviewConversation() {
+//    ComposeExampleTheme {
+//        Conversation(SampleData.conversationSample)
+//    }
+//}
---
+++ app/src/main/java/com/insu/compose/MyApp.kt
@@ -0,0 +1,67 @@
+package com.insu.compose
+
+import android.content.res.Resources
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import com.insu.compose.ui.theme.ComposeExampleTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.CoroutineScope
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun MyApp() {
+    ComposeExampleTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            val appState = rememberAppState()
+
+            Scaffold(
+                scaffoldState = appState.scaffoldState
+            ) { innerPaddingModifier ->
+                NavHost(
+                    navController = appState.navController,
+                    startDestination = SPLASH_SCREEN,
+                    modifier = Modifier.padding(innerPaddingModifier)
+                ) {
+                    myGraph(appState)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun rememberAppState(
+    scaffoldState: ScaffoldState = rememberScaffoldState(),
+    navController: NavHostController = rememberNavController(),
+    snackbarManager: SnackbarManager = SnackbarManager,
+    resources: Resources = resources(),
+    coroutineScope: CoroutineScope = rememberCoroutineScope()
+) =
+    remember(scaffoldState, navController, snackbarManager, resources, coroutineScope) {
+        MakeItSoAppState(scaffoldState, navController, snackbarManager, resources, coroutineScope)
+    }
+
+@Composable
+@ReadOnlyComposable
+fun resources(): Resources {
+    LocalConfiguration.current
+    return LocalContext.current.resources
+}
+
+@ExperimentalMaterialApi
+fun NavGraphBuilder.myGraph(appState: MakeItSoAppState) {
+    composable(SPLASH_SCREEN) {
+        SplashScreen(openAndPopUp = { route, popUp -> appState.navigateAndPopUp(route, popUp) })
+    }
+}
